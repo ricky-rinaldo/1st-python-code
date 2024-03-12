d01604c463bb7cb0e5bb2a660ebcbf0f2b022332
ricky-rinaldo <ricky.rinaldo80@gmail.com>
2024-03-12 14:12:00 +0700

    FOR function Fundamental

---
+++ FOR function.py
@@ -0,0 +1,10 @@
+'''
+for function
+'''
+
+jumlah_buku = 12
+
+jumlah_buku_yang_dibaca = 0
+
+for jumlah_buku_yang_dibaca in range (1,jumlah_buku+1):
+    print (f' Buku ke {jumlah_buku_yang_dibaca}telah dibaca')
