7cbbd939fc4f004a863ac133827de0fbfd1b61d5
ricky-rinaldo <ricky.rinaldo80@gmail.com>
2024-03-13 00:21:48 +0700

    WHILE function with undefined count and stopper variable

---
+++ WHILE function with undefined count and stopper variable.py
@@ -0,0 +1,18 @@
+'''
+Perulangan while dengan variabel pemberhenti
+'''
+
+jumlah_buku = 10
+print ("read all books")
+total_baca = 0
+
+jumlah_paham = 0
+print (f'Jumlah buku yang sudah dibaca dan dipahami = {jumlah_paham}')
+
+while total_baca < jumlah_buku * 2 :
+    total_baca = total_baca + 1
+    if jumlah_paham == 9:
+        print (f'Buku ke {jumlah_paham + 1} belum paham ')
+    else:
+        jumlah_paham = jumlah_paham + 1
+        print (f'Buku ke {total_baca} sudah paham')
